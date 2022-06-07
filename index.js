3de8562c6d180704f4be3948d4e1d2568d979bb4
Victoria LeBel <vickileb@hotmail.com>
2022-06-06 20:28:33 -0700

    commit tree b939472d34ec0edd85a4f2e7cae5365b339ec65a

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1= 31
+let num2= 2
+let num3=19
+let num4=5
+
+const multiply = num1 * num2
+//multiply;
+
+const random = Math.floor(Math.random() *10) +1;
+//random;
+
+const mod = num3 % num4
+//mod;
+
+const max = Math.max(15,16,17,18,19,20)
+//max;
